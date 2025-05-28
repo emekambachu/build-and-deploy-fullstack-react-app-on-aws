9b2d73bc7b39fbf90af3b3c85eb9cea8f04eb542
Victor Emeka Mbachu <victor.c.mbachu@gmail.com>
2025-05-28 14:38:12 +0100

    completed crud fpr frontend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
-const express = require('express');
+import serverless from 'serverless-http';
+import express from 'express';
+import {fetchTasks, createTask, updateTask, deleteTask} from './task.js';
+import bodyParser from "body-parser";
+import cors from "cors";
+
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const port = 3001;
 
 app.use(express.json());
 
-// app.use(cors());
+if(process.env.DEVELOPMENT){
+    app.use(cors());
+}
+
+
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,19 +22,65 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/task', async (req, res) => {
-    res.send('Task!');
+    try{
+        const tasks = await fetchTasks();
+        res.send(tasks.items);
+
+    }catch(err){
+        console.log(err);
+        res.status(400).send("Error fetching tasks | "+err);
+    }
+});
+
+app.post('/task', async (req, res) => {
+    try{
+
+        const task = req.body;
+        const response = await createTask(task);
+        res.send(response);
+
+    }catch(err){
+
+        console.log(err);
+        res.status(400).send("Error creating tasks | "+err);
+
+    }
 });
 
 app.put('/task', async (req, res) => {
-    res.send('Task!');
+
+    try{
+
+        const task = req.body;
+        const response = await updateTask(task);
+        res.send(response);
+
+    }catch(err){
+
+        console.log(err);
+        res.status(400).send("Error updating tasks | "+err);
+
+    }
+
 });
 
-app.delete('/task', async (req, res) => {
-    res.send('Task!');
+app.delete('/task/:id', async (req, res) => {
+
+    try{
+        const {id} = req.params;
+        const response = await deleteTask(id);
+
+    }catch(err){
+        console.log(err);
+        res.status(400).send("Error Deleting Task | "+err);
+    }
+
 });
 
+if(process.env.DEVELOPMENT){
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+}
 
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+export const handler = serverless(app);
